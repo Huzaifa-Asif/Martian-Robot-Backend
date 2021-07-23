e18347c117b993f769859b2715db53589f40e8e8
Huzaifa Asif <huzaifa@theaweb.net>
2021-07-23 15:59:33 +0500

    Express Boilerplate Code

---
+++ app.js
@@ -0,0 +1,59 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongoose = require('mongoose');
+global.__basedir = __dirname;
+
+var cors = require('cors')
+
+var app = express();
+app.use(cors())
+
+// Set up mongoose connection
+let dev_db_url = 'mongodb+srv://usquare:square8580@usquare.h61cw.mongodb.net/USquare-Solutions?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true }).then(() => console.log('MongoDB connected…'))
+.catch(err => console.log(err));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'app_server','views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+
+// Api request limit increase to 50mb for large api request
+app.use(express.json({limit: '50mb', extended: true}));
+app.use(express.urlencoded({limit: '50mb', extended: true }));
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes call
+app.get('/', (req, res) => {
+  res.json({
+    message: "Welcome to Martian Robot Backend"
+  })
+});
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+module.exports = app;
