09cde993e5a4f26516ea9a30209acb0334bc888f
Huzaifa Asif <huzaifa@theaweb.net>
2021-07-23 18:59:33 +0500

    Logic to process Robot Instructions and return the output added

--- app_server/controllers/controller.martinRobot.js
+++ app_server/controllers/controller.martinRobot.js
@@ -1,13 +1,13 @@
-var martinRobot =require('../models/model.martinRobot.js');
+var martinRobotModel =require('../models/model.martinRobot.js');
 
 // Add martinRobot Result
 module.exports.addResult = async (martinRobotform, callback) => {
-    martinRobot.create(martinRobotform, callback);
+    martinRobotModel.create(martinRobotform, callback);
 }
 
 // Get All martinRobot Results 
 module.exports.getAllResults = (callback) => {
-	martinRobot.find(callback);
+	martinRobotModel.find(callback);
 }
 
 
